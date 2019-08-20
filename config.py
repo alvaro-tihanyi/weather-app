12572284f4e6b0124c0ab82f3dafc60430bc5b2f
Álvaro Tihanyi <alvaro-tihanyi@users.noreply.github.com>
2019-08-20 11:09:29 +0200

    Remove config.py from .gitignore.

---
+++ config.py
@@ -0,0 +1,2 @@
+app_id = "b4424222b2e7b22f473ee0bd02793dba"
+default_country_code = "ES"
--- weather.py
+++ weather.py
@@ -7,7 +7,11 @@ import json
 from argument_parser import args
 # the config file contains the api key so it must not 
 # be in the repository
-from config import app_id, default_country_code
+try:
+    from config import app_id, default_country_code
+except ImportError as import_error:
+    print("An error ocurred while trying to import the config file: ")
+    print(import_error)
 
 country_code = default_country_code
 if args.country is not None:
