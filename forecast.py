eb53775b68f8ccebcae172dcac037cfad7c12ffc
Álvaro Tihanyi <alvaro-tihanyi@users.noreply.github.com>
2019-08-21 15:22:47 +0200

    Remove extra code added by accident.

--- forecast.py
+++ forecast.py
@@ -49,25 +49,3 @@ class Forecast:
                 self.city + " (" + self.country + "): " + 
                     weather + ", Temperature " + (str(round(temperature)) + " ºC")
             )
-
-
-
-
-        forecast_json = forecast.json()
-
-        if forecast_json['cod'] != '200':
-            return ({'error': "An error ocurred: (" + forecast_json['cod'] + ") " + forecast_json['message'].capitalize() + "."})
-        else:
-            tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-            weather_list = []
-            temperature_list = []
-
-            # we store only the items that belong to tomorrow
-            for item in forecast_json["list"]:
-                if datetime.date.fromisoformat(item["dt_txt"].split(" ")[0]) == tomorrow:
-                    weather_list.append(item["weather"][0]["main"]) # in the response, weather is a list (although it only has a single element in most cases)
-                    temperature_list.append(item["main"]["temp"])
-            
-            # we get the most frequent weather type and the average temperature for tomorrow
-            weather = self.most_frequent(weather_list)
-            temperature = sum(temperature_list) / len(temperature_list)
